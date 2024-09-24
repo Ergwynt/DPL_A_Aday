4419128367d0bf66b21962b426fe2e6cb0d6564a
Carlos Aday <99324802+Ergwynt@users.noreply.github.com>
2024-09-24 16:02:39 +0200

    Add files via upload

---
+++ practicasXampp/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="get_post.php" method="post" enctype="multipart/form-data">
+        nombre: <input type="text" name="usuario">
+        <br>
+        Fichero: <input type="file" name="Fichero">
+        <br>
+        <input type="submit" name="enviar" value="enviar">
+
+
+    </form>
+</body>
+</html>
